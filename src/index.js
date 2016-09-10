6e12260d0466dda0358e1e9da1549d6a4dfb303d
Ryan Van Etten <ryanve@gmail.com>
2016-09-10 15:55:57 -0700

    space it out a bit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 !function(root, name, make) {
-  if (typeof module != 'undefined' && module.exports) module.exports = make();
-  else root[name] = make();
+  if (typeof module != 'undefined' && module.exports) module.exports = make()
+  else root[name] = make()
 }(this, 'eol', function() {
+
   function converts(to) {
     return function(text) {
-      return text.replace(newline, to);
-    };
+      return text.replace(newline, to)
+    }
   }
-  var api = {}, newline = /\r\n|\r|\n/g;
-  api['lf'] = converts('\n');
-  api['cr'] = converts('\r');
-  api['crlf'] = converts('\r\n');
-  api['auto'] = converts(
-    typeof process != 'undefined' && 'win32' === process.platform ? '\r\n' : '\n'
-  );
-  return api;
+
+  var api = {}
+  var newline = /\r\n|\r|\n/g
+  var isWindows = typeof process != 'undefined' && 'win32' === process.platform
+  
+  api['lf'] = converts('\n')
+  api['cr'] = converts('\r')
+  api['crlf'] = converts('\r\n')
+  api['auto'] = converts(isWindows ? '\r\n' : '\n')
+
+  return api
 });
