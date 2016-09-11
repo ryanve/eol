61183f72936f4dfdd7ba474f3167010e0c5bb57d
Ryan Van Etten <ryanve@gmail.com>
2016-09-10 19:41:56 -0700

    rewrite tests

--- test.js
+++ test.js
@@ -3,30 +3,36 @@
     return !!~str.indexOf(needle);
   }
 
-  var common = typeof module != 'undefined' && !!module.exports;
-  var aok = common ? require('aok') : root.aok;
-  var eol = common ? require('./') : root.eol;
-  var platform = typeof process != 'undefined' && process.platform;
-  var meths = ['lf', 'cr', 'crlf', 'auto'];
-  var chars = ['\n', '\r', '\r\n', 'win32' === platform ? '\r\n' : '\n'];
-  var sample = ' ' + chars.join() + 'text' + chars.join();
+  var common = typeof module != 'undefined' && !!module.exports
+  var aok = common ? require('aok') : root.aok
+  var eol = common ? require('./') : root.eol
+  var platform = typeof process != 'undefined' && process.platform
+  var meths = ['lf', 'cr', 'crlf', 'auto']
+  var chars = ['\n', '\r', '\r\n', 'win32' === platform ? '\r\n' : '\n']
+  var sample = ' ' + chars.join() + 'text' + chars.join()
 
-  aok.fail(meths, function(method, i) {
-    if (!contains('ab', 'a')) return void aok.warn('contains() does not return expected result.');
-    if (!contains(sample, chars[i])) return void aok.warn('Sample lacks the needed characters.');
-    return true;
-  }, aok, 1);
+  aok.prototype.fail = function() {
+    throw new Error('FAILED TEST: ' + this.id)
+  }
+
+  aok('contains() sees contained text', contains('ab', 'a') === true)
+  aok('sample contains newlines', contains(sample, '\n') && contains(sample, '\r'))
+  aok('returns other strings as is', eol.auto('random') === 'random')
+  aok('returns empty strings as is', eol.auto('') === '')
 
   aok.pass(meths, function(method, i) {
-    var normalized = eol[method](sample);
-    aok(method + ' retains', contains(normalized, chars[i]));
+    var normalized = eol[method](sample)
+    aok(method + ' retains', contains(normalized, chars[i]))
     aok(method + ' normalizes', !aok.fail(chars, function(c) {
-      return contains(chars[i], c) === contains(normalized, c);
-    }));
-    return eol.auto(sample) === normalized;
-  });
+      return contains(chars[i], c) === contains(normalized, c)
+    }))
+
+    return eol.auto(sample) === normalized
+  })
 
   aok('auto agrees', 2 === aok.pass(meths, function(method) {
     return eol.auto(sample) === eol[method](sample);
-  }));
+  }))
+
+  aok.log('All tests passed =)')
 }(this);
