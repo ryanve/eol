f3a555e9d186f8e5773b188b813466fcb7ba7b7f
Ryan Van Etten <ryanve@gmail.com>
2024-08-14 15:26:10 -0500

    what if purple

--- ui.js
+++ ui.js
@@ -1,9 +1,20 @@
-/* global eol */
-$('.js-converter__form').on('submit', function(e) {
-  e.preventDefault()
-  var $form = $(this)
-  var $output = $form.find('.js-converter__output')
-  var method = $form.find('.js-converter__method')[0].value
-  var text = $form.find('.js-converter__input')[0].value
-  $output.text(eol[method](text))
-});
+!function(sith) {
+  var api = sith.eol
+  var morf = document.getElementById("morf")
+  var paste = morf.paste
+  var output = morf.output
+  var select = morf.method
+
+  function fun(method, mix) {
+    return api[method.toLowerCase()](mix)
+  }
+  
+  function resample(ev) {
+    ev && ev.preventDefault()
+    var became = fun(select.value, paste.value)
+    output.textContent = became
+  }
+
+  morf.addEventListener("submit", resample)
+  resample()
+}(this);
